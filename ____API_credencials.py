8a1f3cbaf95784a38d4e90742d95fcd0d44ac691
Saul Alvarez <aalvarez1@g-p.com>
2024-04-24 11:34:46 -0600

    creada nueva carpeta

---
+++ ____API_credencials.py
@@ -0,0 +1,7 @@
+
+def get_credentials():
+    class credentials:
+        token_name = 'mi_token'
+        token_secret = 'bB/kYrazTN6cTNCBAyIxaQ==:0Mo8DoDLaKevgqtFT3MOPtj2H8WZ6LC5'
+
+    return credentials
